5f99cf66bf56d938a938c545fae01a160b1b0ef9
Mani sai Y <112387763+Manisai-459@users.noreply.github.com>
2022-11-08 11:16:41 +0530

    Update Tic_Tac_Toe_tinku.py

--- Tic_Tac_Toe_tinku.py
+++ Tic_Tac_Toe_tinku.py
@@ -1,27 +1,21 @@
 import random
-
-
 def valid(ch):
     for i in range(0, len(n)):
-        if i == 0 and n[i] == n[i+4] == n[i+8] == ch:
+        if i == 0 and n[i] == n[i+4] == n[i+8] == ch:  # ! diagonal check - ( 0 , 4 , 8)
             return 1
-        elif i == 2 and n[i] == n[i+2] == n[i+4] == ch:
+        elif i == 2 and n[i] == n[i+2] == n[i+4] == ch:  # ! diagonal check - ( 2 , 4 , 6)
             return 1
-        elif i <= 6 and n[i] == n[i+1] == n[i+2] == ch:   # ! rows
+        elif i <= 6 and n[i] == n[i+1] == n[i+2] == ch:   # ! rows check - ( 0 , 3 , 6)
             return 1
-        elif i <= 2 and n[i] == n[i+3] == n[i+6] == ch:
+        elif i <= 2 and n[i] == n[i+3] == n[i+6] == ch:  # ! column check - ( 0 , 1 , 2)
             return 1
     return 0
-
-
 def ttt(n):
-    l = []
     n1 = n.copy()
-    while(len(l) != 9):
+    while len(n1):
         print("you can choose your choice", n1)
         q = int(input())
         n[q] = "O"
-        l.append(q)
         n1.remove(int(q))
         for i in range(0, len(n), 3):
             print(n[i], "|", n[i+1], "|", n[i+2])
@@ -31,16 +25,13 @@ def ttt(n):
             s = random.choice(n1)
             n[int(s)] = "X"
             n1.remove(s)
-            l.append(s)
         print(f"its computer turn,it placed at {s}")
         for i in range(0, len(n), 3):
             print(n[i], "|", n[i+1], "|", n[i+2])
         if valid('X'):
             return 0
     return -1
-
-
-print("tic tac toe")
+print("Tic Tac Toe")
 n = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 for i in range(0, len(n), 3):
     print(n[i], "|", n[i+1], "|", n[i+2])
